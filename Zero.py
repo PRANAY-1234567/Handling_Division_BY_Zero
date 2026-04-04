0acd9765cdd95c11c701835ad41d7cbce84c36ce
Pranay <pranayvjadhao197@gmail.com>
2026-04-04 22:58:34 +0530

    Initial commit

---
+++ Zero.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("Enter number: "))
+    num2 = int(input("Enter number: "))
+
+    result = num1 / num2
+    print("Result:", result)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
